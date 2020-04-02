0d40c8931d336cc7624d71822bd5513d662f8b69
Mrinal <mrinalkhubchandani13@gmail.com>
2020-04-02 23:09:06 +0530

    done for today and made changes to client.py to submit tomorrow

--- MDL_Assignment_3/client.py
+++ MDL_Assignment_3/client.py
@@ -109,26 +109,29 @@ if __name__ == "__main__":
     data = fd1.readlines()
     fd1.close()
     
-    for i in range(len(data)):
 
-        if i!=0 and data[i] == data[i-1]:
-            continue
+    """ uncomment for reading from file last_iteration.txt """
+    # for i in range(len(data)):
 
-        arr = data[i].split(" ")
-        vec = []
+    #     if i!=0 and data[i] == data[i-1]:
+    #         continue
 
-        for j in range(11):
-            x = float(arr[j])
-            vec.append(x)
+    #     arr = data[i].split(" ")
+    #     vec = []
 
-        trainerror = float(arr[12])
-        valerror = float(arr[13])
-        ind = Individual(vec,trainerror,valerror)
-        population.append(ind)
-        for j in range(9):
-            ind = make_rand(vec)
-            population.append(ind)
+    #     for j in range(11):
+    #         x = float(arr[j])
+    #         vec.append(x)
 
+    #     trainerror = float(arr[12])
+    #     valerror = float(arr[13])
+    #     ind = Individual(vec,trainerror,valerror)
+    #     population.append(ind)
+
+    vec = [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
+    cost = compute_cost(vec)
+    ind = Individual(vec,cost[0],cost[1])
+    
     population = sorted(population,key=lambda x: x.fitness)
     print(len(population))
     for i in range(10):
